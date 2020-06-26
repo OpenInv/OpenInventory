605d87af0204f8b4f7578251f81e2c68ee3d82c3
Chase Carlson <chasedig@gmail.com>
2020-06-25 22:38:48 -0700

    bad update

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 include("header.html");
-echo("testing23")
+echo('hi')
 
 
 ?>
@@ -13,6 +13,36 @@ echo("testing23")
     <title>OpenInventory Home</title>
 </head>
 <body>
-    
+<div class="fixed-action-btn">
+  <a class="btn-floating btn-large red">
+    <i class="large material-icons">mode_edit</i>
+  </a>
+  <ul>
+    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+  </ul>
+</div> 
 </body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<div class="fixed-action-btn">
+  <a ref="#" class="btn-floating red btn-large">
+    <i class="large material-icons">mode_edit</i>
+  </a>
+   <ul>
+        <li><a ref="#" class="btn-floating blue btn-large"><i 
+         class="large material-icons">insert_chart</i></a></li>
+        <li><a ref="#" class="btn-floating green btn-large"><i 
+         class="large material-icons">format_quotes</i></a></li>
+        <li><a ref="#" class="btn-floating yellow btn-large"><i 
+         class="large material-icons">publish</i></a></li>
+        <li><a ref="#" class="btn-floating orange btn-large"><i 
+         class="large material-icons">attach_file</i></a></li>
+    </ul>
+</div>
+</script>
 </html>
